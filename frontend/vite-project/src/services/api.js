92a1fd4b9777901cb0a5cdad29308e339806734a
Ravigopi857 <ravigopi1478@gmail.com>
2026-06-11 18:09:04 +0530

    fixed api url

--- frontend/vite-project/src/services/api.js
+++ frontend/vite-project/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-manager-backend-y60n.onrender.com/",
+  baseURL: "https://task-manager-backend-y60n.onrender.com/api",
 });
 
 export default api;
